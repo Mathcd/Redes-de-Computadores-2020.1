265a4d1ed5512aad804cf1cb6cb40f39823fbceb
Matheus Costa <matheuscosta2004@hotmail.com>
2020-05-28 21:24:19 -0300

    Corrigindo bugs, comentando códigos
    Versão pré-final

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -1,34 +1,25 @@
 # Import socket module
 from socket import *    
-#from flask import Flask, render_template
-
-#app = Flask(__name__, template_folder='.')
-
 
 # Create a TCP server socket
-#(AF_INET is used for IPv4 protocols)
-#(SOCK_STREAM is used for TCP)
-
-#=============================================
-### PRECISA COLOCAR O favicon.ico !!!!!!!!
-#=============================================
-
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Assign a port number
-serverPort = 6788
+serverPort = 6789
 
 # Bind the socket to server address and server port
 serverSocket.bind(("", serverPort))
 
-# Listen to at most 1 connection at a time
-serverSocket.listen(1)
+# Listen to at most 4 connections at a time
+serverSocket.listen(4)
 
 
 # Open and read the requestedFile, send the socket headline
 # according to the http code and send the file through socket
 def read_send(requestedFile, httpCode, connectionSocket):
-	f = open(requestedFile)
+	
+	# open the requested data as binary and read it
+	f = open(requestedFile, 'rb')
 	requestedData = f.read()
 
 	# headline response for socket according to http code
@@ -40,16 +31,14 @@ def read_send(requestedFile, httpCode, connectionSocket):
 	
 	elif httpCode == 500:
 		responseHeader = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
-		
+
+	# Send response header and the requested data		
 	connectionSocket.send(responseHeader)
-
-
-	for ii in range(len(requestedData)):  
-		connectionSocket.send(requestedData[ii])
+	connectionSocket.send(requestedData)
 	connectionSocket.send("\r\n")
 
+	# Close the connection socket
 	connectionSocket.close()
-
 	
 
 # Server should be up and running and listening to the incoming connections
@@ -69,41 +58,55 @@ while True:
 		# The path is the second part of HTTP header, identified by [1]
 		filePath = message.split()[1]
 
-		# ===========================================
-		# KILL SERVER (for development only)
+
+		# Server shutdown command
 		if filePath == '/kill':
-			print ("The server will shutdown now")
+			read_send('serverkilled.html', 200, connectionSocket)
+			connectionSocket.close()
+			print ("The server has been shutdown")
 			break
-		# ===========================================
 		
-		print (filePath)
-		print("Tamanho do filePath:", len(filePath))
 
 		# If no path is specified redirects to default page 
 		if filePath == '/':
-			requestedFile = 'index.htm'
+			requestedFile = 'index.html'
 
+		# Otherwise redirects to the requested file
 		else:
 			requestedFile = filePath[1:]
 
 		# Read and send the file
 		read_send(requestedFile, 200, connectionSocket)
 
+
 	# Exception for file not found
 	except IOError:
-		requestedFile = "error404.htm"
+		requestedFile = "error404.html"
 		read_send(requestedFile, 404, connectionSocket)
 
-		connectionSocket.close()
 
-	# ============================================
-	# PROVISORY! BROKEN PIPE BUG HAS TO BE CORRECTED!!!
+	# Dealing with broken pipe exception bug
 	except IndexError:
-		print ("Houve algum erro de indice\n\n")
-		connectionSocket.send("HTTP/1.1 500 Internal Server Error\r\n\r\n")
-		# connectionSocket.send("<html><head></head><body><h1>500 Internal Server Error</h1></body></html>\r\n")
-	# ============================================
+		requestedFile = "error500.html"
+		read_send(requestedFile, 500, connectionSocket)
+		print ("Internal server error\n\n")
 
 
+	# For unexpected errors
+	except:
+
+		# Try to notify the client with error 500
+		try:
+			requestedFile = "error500.html"
+			read_send(requestedFile, 500, connectionSocket)
+			print ("Internal server error\n\n")
+
+		# If it is not possible to notify the client
+		# just print the error on server's terminal
+		except:
+			print ("Internal server error\n\n")
+
+
+# Close the server socket
 serverSocket.close()  
 
